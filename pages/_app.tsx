8c0b9bde36ea9dbf97aa1b29b4fc266411309a32
MadisonSNYC <madison@thenycagentteam.com>
2025-03-29 22:18:59 -0400

    Comment out unused globals.css import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-import '../styles/globals.css';
+// import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
